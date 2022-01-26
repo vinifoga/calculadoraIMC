f3d28e64b8579da0639aedcb997625e6f5695279
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-26 05:45:41 -0300

    Calculation login

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora IMC"),
+        title: Text("Calculadora IMC "),
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: [
